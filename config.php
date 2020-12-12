b619eb8d35913abe8401127b32b76d58a9baa752
Aristide Mebodo <mebodoaristide@yahoo.fr>
2020-12-12 12:48:01 +0100

    Init
    Push initial

---
+++ config.php
@@ -0,0 +1,45 @@
+<?php
+return array(
+    /**
+     * Provide your MYSQL database credentials
+     */
+    'db_host' => 'localhost',
+    'db_username' => 'u920727527_mood',
+    'db_name' => 'u920727527_mood',
+    'db_password' => 'Mood@27478433',
+    'dbcharacter_encode' => true,
+
+    /**
+     * Option to know if debug is enabled or not
+     */
+    'debug' => true,
+
+    /**
+     * Option to enable https
+     */
+    'https' => false,
+
+    /**
+     * cookie path
+     */
+    'cookie_path' => '/',
+
+    'permalink' => false,
+
+
+    'default_language' => 'fr',
+
+    'api-key' => 'normalKey',
+
+    'rtl-langs' => array(
+        'ar',
+        'fa',
+    ),
+
+    'demo' => true,
+    'site-title' => 'Mood',
+
+    'max-image-size' => '10000000',
+    'image-file-types' => 'jpg,png,gif,jpeg',
+
+);
