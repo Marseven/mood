04bb6eceb53e3c5405109202dec0c3b6464a9fd8
Aristide Mebodo <mebodoaristide@yahoo.fr>
2020-12-31 01:23:23 +0100

    fbk debug

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -719,7 +719,7 @@ class HomeController extends Controller {
         $app_secret = config('facebook-secret');
         $my_url = url('auth/facebook');
         $code = $this->request->input('code');
-
+        die('ici');
         if(empty($code)) {
             $_SESSION['state'] = md5(uniqid(rand(), TRUE));
             $dialog_url = "http://www.facebook.com/dialog/oauth?client_id=".$app_id."&redirect_uri=".urlencode($my_url)."&scope=email&state=".$_SESSION['state'];
